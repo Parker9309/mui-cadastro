10ff185dd09ee34ec2ea33db316f31bd8753b0c9
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-24 10:27:24 -0300

    feat: adicionado reac-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes';
+
 export function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
-
-export default App;
